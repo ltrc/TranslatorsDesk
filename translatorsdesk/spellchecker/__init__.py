83a6e71c48a17ca9b780688d1b5e026eabd692a4
S.P. Mohanty <spmohanty91@gmail.com>
2015-10-20 21:52:07 +0530

    Disabling spellcheckers temporarily because of weird error on ubuntu

--- translatorsdesk/spellchecker/__init__.py
+++ translatorsdesk/spellchecker/__init__.py
@@ -3,15 +3,23 @@ import aspell
 """
 	Instantiates the aspell dictionaries
 """
-en = aspell.Speller(('lang', 'en'), ('encoding', 'utf-8'))
-hi = aspell.Speller(('lang', 'hi'), ('encoding', 'utf-8'))
-te = aspell.Speller(('lang', 'te'), ('encoding', 'utf-8'))
-ta = aspell.Speller(('lang', 'ta'), ('encoding', 'utf-8'))
-pa = aspell.Speller(('lang', 'pa'), ('encoding', 'utf-8'))
+
+#NOTE: Temporarily commenting out aspell related features
+# because of some funny error on Ubuntu
+"""
+te = aspell.Speller('lang', 'te')
+aspell.AspellSpellerError: The file "/usr/lib/aspell/te.rws" is not in the proper format. Incompatible hash function.
+
+"""
+# en = aspell.Speller(('lang', 'en'), ('encoding', 'utf-8'))
+# hi = aspell.Speller(('lang', 'hi'), ('encoding', 'utf-8'))
+# te = aspell.Speller(('lang', 'te'), ('encoding', 'utf-8'))
+# ta = aspell.Speller(('lang', 'ta'), ('encoding', 'utf-8'))
+# pa = aspell.Speller(('lang', 'pa'), ('encoding', 'utf-8'))
 
 dictionaries = {}
-dictionaries['en'] = en
-dictionaries['hi'] = hi
-dictionaries['te'] = te
-dictionaries['ta'] = ta
-dictionaries['pa'] = pa
+# dictionaries['en'] = en
+# dictionaries['hi'] = hi
+# dictionaries['te'] = te
+# dictionaries['ta'] = ta
+# dictionaries['pa'] = pa
--- translatorsdesk/spellchecker/spellchecker.py
+++ translatorsdesk/spellchecker/spellchecker.py
@@ -4,7 +4,7 @@ import aspell
 	Instantiates the aspell dictionaries
 """
 en = aspell.Speller('lang', 'en')
-hi = aspell.Speller('lang', 'hi')
+# hi = aspell.Speller('lang', 'hi') #Removing aspell-hi temporarily because of some funny error on Ubuntu
 te = aspell.Speller('lang', 'te')
 ta = aspell.Speller('lang', 'ta')
 pa = aspell.Speller('lang', 'pa')
@@ -12,7 +12,7 @@ pa = aspell.Speller('lang', 'pa')
 
 dictionaries = {}
 dictionaries['en'] = en
-dictionaries['hi'] = hi
+# dictionaries['hi'] = hi
 dictionaries['te'] = te
 dictionaries['ta'] = ta
 dictionaries['pa'] = pa
