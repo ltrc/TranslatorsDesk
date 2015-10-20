4fc9d67ae1a1fd3afc7fa7ae448321ef8fd01327
S.P. Mohanty <spmohanty91@gmail.com>
2015-10-21 02:56:35 +0530

    Fix a few bugs in file uploader, and add passing of XLIFF to XLIFF editor

--- translatorsdesk/public/views.py
+++ translatorsdesk/public/views.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 '''Public section, including homepage and signup.'''
 from flask import (Blueprint, request, render_template, flash, url_for,
-                    redirect, session, jsonify, redirect, request, current_app)
+                    redirect, session, jsonify, redirect, request, current_app,
+                    abort)
 from flask.ext.login import login_user, login_required, logout_user
 
 from translatorsdesk.extensions import login_manager
@@ -86,3 +87,73 @@ def upload():
 
 def _allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in current_app.config['ALLOWED_FILE_EXTENSIONS']
+
+
+"""
+    Helper functions for Translate
+"""
+
+"""
+    Checks if a uid, fileName pair exists
+"""
+def fileExists(uid, fileName):
+    filepath = os.path.join(current_app.config['UPLOAD_FOLDER'],  uid, fileName)
+    if os.path.exists(filepath):
+        return True
+    else:
+        return False
+
+"""
+    Checks if the XLIFF file for a uid, fileName pair exists
+    Note: This assumes that the uid and fileName pair exists
+"""
+def fileXLIFFExists(uid, fileName):
+    filepath = os.path.join(current_app.config['UPLOAD_FOLDER'],  uid, fileName+".xlf")
+    if os.path.exists(filepath):
+        return True
+    else:
+        return False
+
+def returnFileData(uid, fileName):
+    filepath = os.path.join(current_app.config['UPLOAD_FOLDER'],  uid, fileName)    
+    f = open(filepath, 'r')
+    data = f.read()
+    f.close()
+    return data
+
+
+"""
+    Handles Computer Assisted Translation of a particular xliff file
+"""
+@blueprint.route('/translate/<uid>/<fileName>/', methods=['GET'])
+def translate(uid, fileName):
+    ##Check if the uid and filename exists
+    if fileExists(uid, fileName):
+        ## Case : fileName is a XLIFF file
+        if fileName.endswith(".xlf"):
+            # return the xliff file as it is
+            data = returnFileData(uid, fileName)
+            if not data:
+                return abort(404)
+            return render_template('public/translate.html',\
+                                    fileName=fileName,
+                                    xliff={'data': unicode(data, "utf-8"), 'src':"hi", 'tgt':"pa"},\
+                                    isXLIFFFileItself=True \
+                                    )
+        elif not fileXLIFFExists(uid, fileName):
+            #Start a job for generation of XLIFF
+            flash("Still processing your document, please hold tight.....", "warning")
+            return render_template('public/translate.html',\
+                                    fileName=fileName,
+                                    xliff=False,\
+                                    isXLIFFFileItself=False)
+        else:
+            data = returnFileData(uid, fileName+".xlf")
+            if not data:
+                return abort(404)
+            return render_template('public/translate.html',\
+                                    fileName=fileName,
+                                    xliff={'data': unicode(data, "utf-8"), 'src':"hi", 'tgt':"pa"},\
+                                    isXLIFFFileItself=False)
+    else:
+        return abort(404)
