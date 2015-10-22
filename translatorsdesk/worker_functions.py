07ed23f19464cd8d159347ccaaea3527d8373fb3
S.P. Mohanty <spmohanty91@gmail.com>
2015-10-23 02:39:32 +0530

    Add redis based state tracking for conversion operations

--- translatorsdesk/worker_functions.py
+++ translatorsdesk/worker_functions.py
@@ -7,22 +7,38 @@ redis_conn = Redis()
 q = Queue(connection=redis_conn)
 
 
+def get_redis_connection():
+    return Redis()
+
+def change_state(file, state):
+    print "="*80, state
+    r_conn = get_redis_connection()
+    u_file = "/".join(file.split("/")[-2:])
+
+    r_conn.lpush("state_"+u_file, state)
+
 #=================================================================
 # Process Input File
 
 def extract_xliff(file):
-	cmd = ["lib/okapi/tikal.sh", "-x", file]
-	p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
+    change_state(file,"EXTRACTING_XLIFF")
+    cmd = ["lib/okapi/tikal.sh", "-x", file]
+    p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
 							stderr=subprocess.PIPE,
 							stdin=subprocess.PIPE)
-	out, err = p.communicate()
+    out, err = p.communicate()
+    change_state(file,"EXTRACTING_XLIFF:::COMPLETE")
+
 
 def extract_po(file):
-	cmd = ["xliff2po", "-i", file+".xlf", "-o", file+".po"]
-	p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
+    change_state(file,"EXTRACTING_PO")
+
+    cmd = ["xliff2po", "-i", file+".xlf", "-o", file+".po"]
+    p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
 							stderr=subprocess.PIPE,
 							stdin=subprocess.PIPE)
-	out, err = p.communicate()	
+    out, err = p.communicate()	
+    change_state(file,"EXTRACTING_PO:::COMPLETE")    
 
 
 def process_input_file(file):
@@ -43,21 +59,25 @@ def newFilePath(fileName):
 
     newPath = fileName
     newPath = newPath.split("/")[:-1]
-    newPath.append(newFileName)
+    newPath.append(newFileName.split("/")[-1])
     newPath = "/".join(newPath)
 
     return newPath
 
 def mergePOFileWithXLF(file):
-		#Merge PO file onto XLIFF File
+    change_state(file,"MERGING_PO_WITH_XLIFF")    
+	#Merge PO file onto XLIFF File
     cmd = ["pomerge", "-i", file+".po", "-t", file+".xlf", "-o", file+".xlf.new"]
     p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
                             stderr=subprocess.PIPE,
                             stdin=subprocess.PIPE)
     out, err = p.communicate()
     print cmd, out, err	
+    change_state(file,"MERGING_PO_WITH_XLIFF:::COMPLETE")    
+
 
 def takeBackupOfOldXLFFile(file):
+    change_state(file,"TAKING_BACKUP_OF_OLD_XLIFF")      
     #Move old xlf file to a new location and mark it as .old
     cmd = ["mv", file+".xlf", file+".xlf.old"]
     p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
@@ -65,8 +85,11 @@ def takeBackupOfOldXLFFile(file):
                             stdin=subprocess.PIPE)
     out, err = p.communicate()
     print cmd, out, err
+    change_state(file,"TAKING_BACKUP_OF_OLD_XLIFF:::COMPLETE")      
+
 
 def moveNewXLFToCorrectLocation(file):
+    change_state(file,"UPDATE_XLF")       
     #Move the newly generated xlf file to the expected location of xlf file
     cmd = ["mv", file+".xlf.new", file+".xlf"]
     p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
@@ -74,8 +97,11 @@ def moveNewXLFToCorrectLocation(file):
                             stdin=subprocess.PIPE)
     out, err = p.communicate()
     print cmd, out, err
+    change_state(file,"UPDATE_XLF:::COMPLETE")           
 
 def removeOldOutputFile(file):
+    change_state(file,"REMOVE_OLD_OUTPUT")
+
     newPath = newFilePath(file)
     #Delete any old generated files, if present
     cmd = ["rm", newPath]
@@ -84,8 +110,12 @@ def removeOldOutputFile(file):
                             stdin=subprocess.PIPE)
     out, err = p.communicate()
     print cmd, out, err     
+    change_state(file,"REMOVE_OLD_OUTPUT:::COMPLETE")
+
 
 def mergeTranslatedXLFFileWithDocument(file):
+    change_state(file,"MERGE_TRANSLATED_XLIFF_FILE")
+
     #Merge translated xlf file with the doc
     cmd = ["lib/okapi/tikal.sh", "-m", file+".xlf"]
     p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
@@ -93,23 +123,21 @@ def mergeTranslatedXLFFileWithDocument(file):
                             stdin=subprocess.PIPE)
     out, err = p.communicate()
     print cmd, out, err
+    change_state(file,"MERGE_TRANSLATED_XLIFF_FILE:::COMPLETE")
 
-def generateOutputFile(file):
-		# file : fullpath of the file
 
-		mergePOFileWithXLF(file)
-		takeBackupOfOldXLFFile(file)
-		moveNewXLFToCorrectLocation(file)
-		removeOldOutputFile(file)
-		mergeTranslatedXLFFileWithDocument(file)
+def generateOutputFile(file): 
+	# file : fullpath of the file
 
-		newPath = newFilePath(file)
-		publicly_accessible_path = "/" + "/".join(newPath.split("/")[1:])
- 
+    change_state(file, "BEGIN_PROCESSING_OF_FILE")
 
-def merge(xliff_file):
-	cmd = ["lib/okapi/tikal.sh", "-m", xliff_file]
-	p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
-							stderr=subprocess.PIPE,
-							stdin=subprocess.PIPE)
-	out, err = p.communicate()
+    mergePOFileWithXLF(file)
+    takeBackupOfOldXLFFile(file)
+    moveNewXLFToCorrectLocation(file)
+    removeOldOutputFile(file)
+    mergeTranslatedXLFFileWithDocument(file)
+
+    newPath = newFilePath(file)
+    publicly_accessible_path = "/" + "/".join(newPath.split("/")[1:])
+
+    change_state(file, "OUTPUT_FILE_GENERATED:::"+publicly_accessible_path)
