6b9d9dece0e0b2ed0b3e3dc71180f6cb8d515e42
Mrinal Dhar <mrinal.dhar@gmail.com>
2016-04-05 15:48:41 +0530

    final commit before demo

--- translatorsdesk/static/sepjs/poeditor.js
+++ translatorsdesk/static/sepjs/poeditor.js
@@ -240,7 +240,7 @@ function fix_highlighting() {
 function response_word_suggestion(data) {
 	data = JSON.parse(data);
 	console.log(data);
-	$('#modal_details').html("<big>"+data["word"]+": ( "+data["cat"]+" ) "+data["meaning"]+"<br /><i>Example: \""+data["example"]+"\"</i><br />Alternate translations: "+data["alternate"].join(" &bull; "));
+	$('#modal_details').html("<br /><big>"+data["word"]+": ( "+data["cat"]+" ) "+data["meaning"]+"<br /><i>Example: \""+data["example"]+"\"</i><br />Alternate translations: "+data["alternate"].join(" &bull; "));
 }
 
 function setup_modal() {
